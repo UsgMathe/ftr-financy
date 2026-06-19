236683b191a3278f3230bac59cc44f9ca281ee25
Matheus Gabriel Valenga <theusgvalenga@outlook.com>
2026-06-18 23:48:54 -0300

    chore: add components example to the "app" component

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,7 +1,9 @@
 import { Logo, LogoIcon } from "@/assets";
-import { MailIcon } from "lucide-react";
+import { MailIcon, TrashIcon, UserPlus2Icon } from "lucide-react";
 import { InputField } from "./components/input-field";
 import { SelectField } from "./components/select-field";
+import { Badge } from "./components/ui/badge";
+import { Button } from "./components/ui/button";
 
 export function App() {
   return (
@@ -43,6 +45,7 @@ export function App() {
       <SelectField
         label="Label"
         placeholder="Select a fruit"
+        helper="Helper"
         items={[
           { value: "1", label: "Option 1" },
           { value: "2", label: "Option 2" },
@@ -50,6 +53,75 @@ export function App() {
         ]}
         onValueChange={console.log}
       />
+
+      <div className="grid w-fit grid-cols-2 gap-10">
+        <Button>
+          <UserPlus2Icon /> Label
+        </Button>
+
+        <Button variant="outline">
+          <UserPlus2Icon /> Label
+        </Button>
+
+        <Button disabled>
+          <UserPlus2Icon /> Label
+        </Button>
+
+        <Button disabled variant="outline">
+          <UserPlus2Icon /> Label
+        </Button>
+      </div>
+
+      <div className="grid w-fit grid-cols-2 gap-10">
+        <Button size="sm">
+          <UserPlus2Icon /> Label
+        </Button>
+
+        <Button size="sm" variant="outline">
+          <UserPlus2Icon /> Label
+        </Button>
+
+        <Button disabled size="sm">
+          <UserPlus2Icon /> Label
+        </Button>
+
+        <Button disabled size="sm" variant="outline">
+          <UserPlus2Icon /> Label
+        </Button>
+      </div>
+
+      <div className="grid w-fit grid-cols-2 gap-10">
+        <Button size="icon" variant="outline">
+          <UserPlus2Icon />
+        </Button>
+
+        <Button size="icon" variant="outline-destructive">
+          <TrashIcon />
+        </Button>
+
+        <Button disabled size="icon" variant="outline">
+          <UserPlus2Icon />
+        </Button>
+
+        <Button disabled size="icon" variant="outline-destructive">
+          <TrashIcon />
+        </Button>
+      </div>
+
+      <div>
+        <a href="">Label</a>
+      </div>
+
+      <div className="grid w-fit grid-cols-4 gap-10">
+        <Badge>Label</Badge>
+        <Badge variant="blue">Label</Badge>
+        <Badge variant="purple">Label</Badge>
+        <Badge variant="pink">Label</Badge>
+        <Badge variant="red">Label</Badge>
+        <Badge variant="orange">Label</Badge>
+        <Badge variant="yellow">Label</Badge>
+        <Badge variant="green">Label</Badge>
+      </div>
     </div>
   );
 }
