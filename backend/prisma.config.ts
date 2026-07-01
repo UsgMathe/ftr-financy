c4f3033b19615b4be3c2e83e06f3169b85223cd5
Matheus Gabriel Valenga <theusgvalenga@outlook.com>
2026-06-30 22:47:52 -0300

    refactor(backend): remove unused prisma seed files and scripts

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   schema: './src/prisma/schema.prisma',
   migrations: {
     path: './src/prisma/migrations',
-    seed: 'tsx ./src/prisma/seed.ts',
   },
   datasource: {
     url: process.env['DATABASE_URL'],
